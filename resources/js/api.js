ff3d153e38dc2f48aa559b7b6e71f30c61ea2a03
Zac Bruce <zac@tuft.co.nz>
2019-08-29 22:09:08 +1200

    Fix API with static methods.

--- resources/js/api.js
+++ resources/js/api.js
@@ -2,10 +2,27 @@ import axios from 'axios';
 /**
  * Submits our HTTP requests do send/receive things on the server side.
  */
-export const historyFetch = () => {
-    return axios.get('allhistory');
-};
+export class History{
+    static get() {
+        return axios.get('allhistory');
+    };
+}
 
-export const store = (data) => {
-    return axios.post('todo', data)
-};
+export class Items{
+    static get() {
+        return axios.get('todo');
+    }
+    static create(data) {
+        return axios.post('todo', data);
+    }
+    static edit(item_id, data){
+        return axios.patch('todo/' + item_id, data);
+    }
+    static delete(item_id){
+        return axios.delete('todo/' + item_id)
+    }
+    static clear(){
+        return axios.delete('todo');
+    }
+
+}
